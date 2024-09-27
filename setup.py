b9ebc07e2371ad0cd7c9778d2c001d95bc31f049
Luis Felipe Vendramim <luisfelipe100@gmail.com>
2024-09-27 08:15:09 -0300

    Update setup.py
    Update informations

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="first_package",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Luis_Felipe_Vendramim",
+    author_email="luisfelipe@gmail.com",
+    description="Desafio Projeto DIO",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/luisfelipevendramim/simple-package-template/"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
